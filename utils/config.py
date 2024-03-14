22533b359dd8ba7d5b70fc49f660928d5c567ac9
jcAlpaca <142179083+jcAlpaca@users.noreply.github.com>
2024-03-14 14:17:55 -0600

    working on retrieving data for the past day, have requests working

--- utils/config.py
+++ utils/config.py
@@ -1,3 +1,6 @@
-API_KEY = 'PKTZPDFB4YZOOJGBICQL'
-API_SECRET = 'ErZeHEVKZinf4BuBY3aNukEjcrJaxkzQX5HL8fGX'
+API_KEY = 'PK3ZBV6FA4F2UPH4XAA6'
+API_SECRET = 'HpV9SV7WAaAllhOBmgV75AxvxvgDcV9kOscCHz3x'
 ENDPOINT_URL = 'https://paper-api.alpaca.markets/v2'
+ACCOUNT_URL = "{}/account".format(ENDPOINT_URL)
+ORDERS_URL = "{}/orders".format(ENDPOINT_URL)
+BARS_URL = "https://data.alpaca.markets/v2/stocks/bars"
